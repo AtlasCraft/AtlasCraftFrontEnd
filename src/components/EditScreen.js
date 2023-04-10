fc652faeee583ae1908c180732bafaf34054b921
Jason Zheng <jaszheng2001@gmail.com>
2023-04-09 23:34:59 -0400

    finish edit screen layout

--- src/components/EditScreen.js
+++ src/components/EditScreen.js
@@ -8,13 +8,55 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Grid from '@mui/material/Grid';
+import { MapContainer, GeoJSON, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+import 'leaflet/dist/leaflet.css';
+import Input from '@mui/material/Input';
+import EditIcon from '@mui/icons-material/Edit';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import MergeIcon from '@mui/icons-material/Merge';
+import CallSplitIcon from '@mui/icons-material/CallSplit';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
 
+const mapData = require('../test/MapEditingInfo.json');
 export default function EditScreen() {
   const [value, setValue] = React.useState('1');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const countryStyle = {
+    fillColor: 'yellow',
+    color: 'black',
+    weight: 1,
+    height: '100%',
+  };
+
+  const editIconBoxStyle = {
+    width: '100%',
+    'aspect-ratio': '1 / 1',
+    backgroundColor: 'rgb(200,200,200)',
+    'border-radius': '10px',
+    display: 'flex',
+    'flex-direction': 'column',
+    'justify-content': 'center',
+    'align-items': 'center',
+  };
+
+  const editIconStyle = {
+    fontSize: 50,
+    'text-align': 'center',
+  };
+
+  const editTextStyle = {
+    margin: '.5rem 0',
+    'text-align': 'center',
+  };
+  console.log(mapData);
   return (
     <div>
       <div>
@@ -36,7 +78,12 @@ export default function EditScreen() {
             alignItems="center"
             spacing={2}
           >
-            <TextField id="mapname" label="Name" variant="filled" hiddenLabel />
+            <TextField
+              id="mapname"
+              variant="filled"
+              value={mapData.mapName}
+              hiddenLabel
+            />
             <Button
               variant="contained"
               href="#"
@@ -60,21 +107,36 @@ export default function EditScreen() {
         <Stack direction="row" height="100%">
           <div
             style={{
-              width: '75%',
+              width: '80%',
               background: 'gray',
               height: '100%',
             }}
           >
-            Map
+            <MapContainer
+              style={{ height: '80vh' }}
+              center={[42.09618442380296, -71.5045166015625]}
+              zoom={7}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <GeoJSON data={mapData.geojson} style={countryStyle} />
+            </MapContainer>
           </div>
-          <div style={{ width: '25%', background: 'white' }}>
+          <div style={{ width: '20%', background: 'white', height: '100%' }}>
             <TabContext value={value}>
               <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <TabList onChange={handleChange} aria-label="Edit Tabs">
+                <TabList
+                  onChange={handleChange}
+                  aria-label="Edit Tabs"
+                  variant="scrollable"
+                  scrollButtons={false}
+                >
                   <Tab label="Edit" value="1" />
                   <Tab label="Properties" value="2" />
                   <Tab label="Upload" value="3" />
-                  <Tab label="Help" value="3" />
+                  <Tab label="Help" value="4" />
                 </TabList>
               </Box>
               <TabPanel value="1">
@@ -85,22 +147,95 @@ export default function EditScreen() {
                     columnSpacing={{ xs: 1, sm: 2, md: 3 }}
                   >
                     <Grid item xs={6}>
-                      <p>1</p>
+                      <Box sx={editIconBoxStyle}>
+                        <EditIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>EDIT</p>
+                      </Box>
                     </Grid>
                     <Grid item xs={6}>
-                      <p>2</p>
+                      <Box sx={editIconBoxStyle}>
+                        <LibraryAddIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>CREATE REGION</p>
+                      </Box>
                     </Grid>
                     <Grid item xs={6}>
-                      <p>3</p>
+                      <Box sx={editIconBoxStyle}>
+                        <MergeIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>MERGE REGION</p>
+                      </Box>
                     </Grid>
                     <Grid item xs={6}>
-                      <p>4</p>
+                      <Box sx={editIconBoxStyle}>
+                        <CallSplitIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>SPLIT REGION</p>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Box sx={editIconBoxStyle}>
+                        <UndoIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>UNDO</p>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Box sx={editIconBoxStyle}>
+                        <RedoIcon sx={editIconStyle} />
+                        <p style={editTextStyle}>REDO</p>
+                      </Box>
                     </Grid>
                   </Grid>
                 </Box>
               </TabPanel>
-              <TabPanel value="2">Item Two</TabPanel>
-              <TabPanel value="3">Item Three</TabPanel>
+              <TabPanel value="2">
+                <div style={{ display: 'flex', 'flex-direction': 'column' }}>
+                  <TextField
+                    id="propForm"
+                    variant="filled"
+                    placeholder="name: value"
+                    hiddenLabel
+                  />
+                  <a>+ Add property</a>
+                </div>
+              </TabPanel>
+              <TabPanel value="3">
+                <div>
+                  <label htmlFor="upload GEOJSON">
+                    <input
+                      style={{ display: 'none' }}
+                      id="upload-geojson"
+                      name="upload-geojson"
+                      type="file"
+                    />
+
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      component="span"
+                    >
+                      Upload GEOJSON
+                    </Button>
+                  </label>
+                  <label htmlFor="upload Shapefile">
+                    <input
+                      style={{ display: 'none' }}
+                      id="upload-shapefile"
+                      name="upload-shapefile"
+                      type="file"
+                    />
+
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      component="span"
+                      width="100%"
+                    >
+                      Upload Shapefile
+                    </Button>
+                  </label>
+                </div>
+              </TabPanel>
+              <TabPanel value="4">
+                <a>How to add vertices</a>
+              </TabPanel>
             </TabContext>
           </div>
         </Stack>
