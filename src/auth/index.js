59689727f5fe77dbda73655e8cc7b7678685d29a
Jason Zheng <jaszheng2001@gmail.com>
2023-04-16 16:57:11 -0400

    implement register and login screen logic

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,150 +1,149 @@
-import React, { createContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import api from "../api";
+import React, { createContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../api';
 
 const AuthContext = createContext();
-console.log("create AuthContext: " + AuthContext);
+console.log('create AuthContext: ' + AuthContext);
 
 // THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
 export const AuthActionType = {
-	GET_LOGGED_IN: "GET_LOGGED_IN",
-	REGISTER_USER: "REGISTER_USER",
-	LOGIN_USER: "LOGIN_USER",
-	LOGOUT_USER: "LOGOUT_USER",
+  GET_LOGGED_IN: 'GET_LOGGED_IN',
+  REGISTER_USER: 'REGISTER_USER',
+  LOGIN_USER: 'LOGIN_USER',
+  LOGOUT_USER: 'LOGOUT_USER',
 };
 
 function AuthContextProvider(props) {
-	const [auth, setAuth] = useState({
-		user: null,
-		loggedIn: false,
-		error: false,
-		errorMsg: null,
-		// guest: false,
-	});
-	const history = useHistory();
+  const [auth, setAuth] = useState({
+    user: null,
+    loggedIn: false,
+    error: false,
+    errorMsg: null,
+    // guest: false,
+  });
+  const history = useHistory();
 
-	useEffect(() => {
-		console.log("Auth User Effect");
-		auth.getLoggedIn();
-	}, []);
+  useEffect(() => {
+    console.log('Auth User Effect');
+    auth.getLoggedIn();
+  }, []);
 
-	const authReducer = (action) => {
-		const { type, payload } = action;
-		switch (type) {
-			case AuthActionType.GET_LOGGED_IN: {
-				return setAuth({
-					user: payload.user,
-					loggedIn: payload.loggedIn,
-					error: false,
-					errorMsg: null,
-					// guest: auth.guest,
-				});
-			}
-			case AuthActionType.LOGIN_USER: {
-				return setAuth({
-					user: payload.user,
-					loggedIn: true,
-					error: false,
-					errorMsg: null,
-					// guest: false,
-				});
-			}
-			case AuthActionType.LOGOUT_USER: {
-				return setAuth({
-					user: null,
-					loggedIn: false,
-					error: false,
-					errorMsg: null,
-					// guest: false,
-				});
-			}
-			default:
-				return auth;
-		}
-	};
+  const authReducer = (action) => {
+    const { type, payload } = action;
+    switch (type) {
+      case AuthActionType.GET_LOGGED_IN: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: payload.loggedIn,
+          error: false,
+          errorMsg: null,
+          // guest: auth.guest,
+        });
+      }
+      case AuthActionType.LOGIN_USER: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: true,
+          error: false,
+          errorMsg: null,
+          // guest: false,
+        });
+      }
+      case AuthActionType.LOGOUT_USER: {
+        return setAuth({
+          user: null,
+          loggedIn: false,
+          error: false,
+          errorMsg: null,
+          // guest: false,
+        });
+      }
+      default:
+        return auth;
+    }
+  };
 
-	auth.getLoggedIn = async function () {
-		try {
-			const response = await api.getLoggedIn();
-			console.log(response);
-			if (response.status === 200) {
-				authReducer({
-					type: AuthActionType.GET_LOGGED_IN,
-					payload: {
-						loggedIn: response.data.loggedIn,
-						user: response.data.user,
-					},
-				});
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  auth.getLoggedIn = async function () {
+    try {
+      const response = await api.getLoggedIn();
+      console.log(response);
+      if (response.status === 200) {
+        authReducer({
+          type: AuthActionType.GET_LOGGED_IN,
+          payload: {
+            loggedIn: response.data.loggedIn,
+            user: response.data.user,
+          },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	auth.registerUser = async function (userData, store) {
-		try {
-			const response = await api.registerUser(userData);
-			if (response.status === 200) {
-				history.push("/");
-			}
-		} catch (err) {
-			store.showErr(err.response.status, err.response.data.errorMessage);
-		}
-	};
+  auth.registerUser = async function (userData, store) {
+    try {
+      const response = await api.registerUser(userData);
+      if (response.status === 200) {
+        history.push('/');
+      }
+    } catch (err) {
+      store.showErr(err.response.status, err.response.data.errorMessage);
+    }
+  };
 
-	auth.loginUser = async function (userData, store) {
-		try {
-			const response = await api.loginUser(userData);
-			if (response.status === 200) {
-				authReducer({
-					type: AuthActionType.LOGIN_USER,
-					payload: {
-						user: response.data.user,
-					},
-				});
-				store.loadMapCards();
-				history.push("/home");
-				
-			}
-		} catch (err) {
-			store.showErr(err.response.status, err.response.data.errorMessage);
-		}
-	};
+  auth.loginUser = async function (userData, store) {
+    try {
+      const response = await api.loginUser(userData);
+      if (response.status === 200) {
+        authReducer({
+          type: AuthActionType.LOGIN_USER,
+          payload: {
+            user: response.data.user,
+          },
+        });
+        store.loadMapCards();
+        history.push('/home');
+      }
+    } catch (err) {
+      store.showErr(err.response.status, err.response.data.errorMessage);
+    }
+  };
 
-	auth.logoutUser = async function (store) {
-		store.closeCurrentList();
-		try {
-			const response = await api.logoutUser();
-			if (response.status === 200) {
-				authReducer({
-					type: AuthActionType.LOGOUT_USER,
-					payload: {},
-				});
-			}
-			history.push("/");
-		} catch (err) { }
-	};
+  auth.logoutUser = async function (store) {
+    store.closeCurrentList();
+    try {
+      const response = await api.logoutUser();
+      if (response.status === 200) {
+        authReducer({
+          type: AuthActionType.LOGOUT_USER,
+          payload: {},
+        });
+      }
+      history.push('/');
+    } catch (err) {}
+  };
 
-	auth.changePassword = async function (store) {
-		//For build 4
-	};
+  auth.changePassword = async function (store) {
+    //For build 4
+  };
 
-	auth.fakeLogin = async function () {
-		setAuth({
-			user: { firstName: 'Atlas', lastName: 'Craft' },
-			loggedIn: true
-		});
-		history.push("/home");
-	};
-	return (
-		<AuthContext.Provider
-			value={{
-				auth,
-			}}
-		>
-			{props.children}
-		</AuthContext.Provider>
-	);
+  auth.fakeLogin = async function () {
+    setAuth({
+      user: { firstName: 'Atlas', lastName: 'Craft' },
+      loggedIn: true,
+    });
+    history.push('/home');
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthContext;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createContext, useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
-import api from "../api";
-import jsTPS from "../common/jsTPS"
-import AuthContext from "../auth";
+import { createContext, useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../api';
+import jsTPS from '../common/jsTPS';
+import AuthContext from '../auth';
 /*
 	This is our global data store. Note that it uses the Flux design pattern,
 	which makes use of things like actions and reducers. 
@@ -16,169 +16,161 @@ export const GlobalStoreContext = createContext({});
 // THESE ARE ALL THE TYPES OF UPDATES TO OUR GLOBAL
 // DATA STORE STATE THAT CAN BE PROCESSED
 export const GlobalStoreActionType = {
-	SET_MAP : "SET_MAP",
-	DELETE_VERT: "DELETE_VERT",
-	ADD_VERT: "ADD_VERT",
-	MOVE_VERT: "MOVE_VERT",
-	MERGE_REGION: "MERGE_REGION",
-	SPLIT_REGION: "SPLIT_REGION",
-	CREATE_REGION: "CREATE_REGION",
-	SET_SELECTED_REGION: "SET_SELECTED_REGION",
-	ADD_PROP: "ADD_PROP",
-	UPDATE_PROP: "UPDATE_PROP",
-	SET_MAPCARDS:"SET_MAPCARDS",
+  SET_MAP: 'SET_MAP',
+  DELETE_VERT: 'DELETE_VERT',
+  ADD_VERT: 'ADD_VERT',
+  MOVE_VERT: 'MOVE_VERT',
+  MERGE_REGION: 'MERGE_REGION',
+  SPLIT_REGION: 'SPLIT_REGION',
+  CREATE_REGION: 'CREATE_REGION',
+  SET_SELECTED_REGION: 'SET_SELECTED_REGION',
+  ADD_PROP: 'ADD_PROP',
+  UPDATE_PROP: 'UPDATE_PROP',
+  SET_MAPCARDS: 'SET_MAPCARDS',
+  SHOW_ERROR: 'SHOW_ERROR',
+  HIDE_ERR: 'HIDE_ERR',
 };
 
 // WITH THIS WE'RE MAKING OUR GLOBAL DATA STORE
 // AVAILABLE TO THE REST OF THE APPLICATION
 function GlobalStoreContextProvider(props) {
-	// THESE ARE ALL THE THINGS OUR DATA STORE WILL MANAGE
-	const [store, setStore] = useState({
-		geojson: null,
-		nameMap: null,
-		mapId: null,
-		ownedUser: null,
-		mapcardList: [],
-		ownedMapCardList:[],
-		editable: false,
-		graphicState:[],
-		selectedRegion: [],
-		regionProperty: null,
-		editSelection: null,
-	});
-	const history = useHistory();
+  // THESE ARE ALL THE THINGS OUR DATA STORE WILL MANAGE
+  const [store, setStore] = useState({
+    geojson: null,
+    nameMap: null,
+    mapId: null,
+    ownedUser: null,
+    mapcardList: [],
+    ownedMapCardList: [],
+    editable: false,
+    graphicState: [],
+    selectedRegion: [],
+    regionProperty: null,
+    editSelection: null,
+  });
+  const history = useHistory();
 
-	const tps = new jsTPS();
-	
-	// SINCE WE'VE WRAPPED THE STORE IN THE AUTH CONTEXT WE CAN ACCESS THE USER HERE
-	const { auth } = useContext(AuthContext);
+  const tps = new jsTPS();
 
-	// HERE'S THE DATA STORE'S REDUCER, IT MUST
-	// HANDLE EVERY TYPE OF STATE CHANGE
-	const storeReducer = (action) => {
-		const { type, payload } = action;
-		switch (type) {
-			// LIST UPDATE OF ITS NAME
-			case GlobalStoreActionType.SET_MAPCARDS: {
-				return setStore({
-					geojson: null,
-					nameMap: null,
-					mapId: null,
-					ownedUser: null,
-					mapcardList: payload,
-					ownedMapCardList:[],
-					editable: false,
-					graphicState:[],
-					selectedRegion: [],
-					regionProperty: null,
-					editSelection: null,
-				});
-			}
-			default:
-				return store;
-		}
-	};
+  // SINCE WE'VE WRAPPED THE STORE IN THE AUTH CONTEXT WE CAN ACCESS THE USER HERE
+  const { auth } = useContext(AuthContext);
 
-	// THESE ARE THE FUNCTIONS THAT WILL UPDATE OUR STORE AND
-	// DRIVE THE STATE OF THE APPLICATION. WE'LL CALL THESE IN
-	// RESPONSE TO EVENTS INSIDE OUR COMPONENTS.
+  // HERE'S THE DATA STORE'S REDUCER, IT MUST
+  // HANDLE EVERY TYPE OF STATE CHANGE
+  const storeReducer = (action) => {
+    const { type, payload } = action;
+    switch (type) {
+      // LIST UPDATE OF ITS NAME
+      case GlobalStoreActionType.SET_MAPCARDS: {
+        return setStore({
+          geojson: null,
+          nameMap: null,
+          mapId: null,
+          ownedUser: null,
+          mapcardList: payload,
+          ownedMapCardList: [],
+          editable: false,
+          graphicState: [],
+          selectedRegion: [],
+          regionProperty: null,
+          editSelection: null,
+        });
+      }
+      case GlobalStoreActionType.SHOW_ERR: {
+        console.log(payload);
+        return setStore({
+          ...store,
+          err: payload,
+        });
+      }
+      case GlobalStoreActionType.HIDE_ERR: {
+        return setStore({
+          ...store,
+          err: null,
+        });
+      }
+      default:
+        return store;
+    }
+  };
 
-	// ***ANY FUNCTION NOT FILLED IN MEANS IT IS PLANNED FOR A FUTURE BUILD***
+  // THESE ARE THE FUNCTIONS THAT WILL UPDATE OUR STORE AND
+  // DRIVE THE STATE OF THE APPLICATION. WE'LL CALL THESE IN
+  // RESPONSE TO EVENTS INSIDE OUR COMPONENTS.
 
-	store.loadMapCards = async function(){
-		let res = await api.getAllMapCards();
-		if(res.data.success){
-			console.log(res.data);
-		}
-	}
+  // ***ANY FUNCTION NOT FILLED IN MEANS IT IS PLANNED FOR A FUTURE BUILD***
 
-	// tps handling functions
-	store.canUndo = function(){
-		return tps.hasTransactionToUndo();
-	}
-	store.canRedo = function(){
-		return tps.hasTransactionToRedo();
-	}
-	store.undo = function(){
-		tps.undoTransaction();	
-	}
-	store.redo = function(){
-		tps.redoTransaction();	
-	}
+  store.loadMapCards = async function () {
+    let res = await api.getAllMapCards();
+    if (res.data.success) {
+      console.log(res.data);
+    }
+  };
 
-	//region functions
-	store.deleteRegion = function(){
+  // tps handling functions
+  store.canUndo = function () {
+    return tps.hasTransactionToUndo();
+  };
+  store.canRedo = function () {
+    return tps.hasTransactionToRedo();
+  };
+  store.undo = function () {
+    tps.undoTransaction();
+  };
+  store.redo = function () {
+    tps.redoTransaction();
+  };
 
-	}
-	store.createRegion = function(){
+  //region functions
+  store.deleteRegion = function () {};
+  store.createRegion = function () {};
+  store.mergeRegion = function () {};
+  store.splitRegion = function () {};
+  store.selectRegion = function () {};
 
-	}
-	store.mergeRegion = function(){
+  //verticies functions
+  store.deleteVertices = function () {};
+  store.addVertices = function () {};
+  store.moveVertices = function () {};
+  store.selectVertices = function () {};
 
-	}
-	store.splitRegion = function(){
+  //Properties functions
+  store.updateProp = function () {};
+  store.createProp = function () {};
 
-	}
-	store.selectRegion = function(){
+  //map management
+  store.downloadGeo = function () {};
+  store.downloadShp = function () {};
+  store.downloadPng = function () {};
+  store.uploadMap = function () {};
+  store.forkMap = function () {};
+  store.changeMapName = function () {};
+  store.saveMap = function () {};
+  store.deleteMap = function () {};
+  store.showErr = function (statusCode, msg) {
+    console.log(msg);
+    storeReducer({
+      type: GlobalStoreActionType.SHOW_ERR,
+      payload: `Error ${statusCode}: ${msg}`,
+    });
+  };
 
-	}
+  store.hideErr = function () {
+    storeReducer({
+      type: GlobalStoreActionType.HIDE_ERR,
+      payload: null,
+    });
+  };
 
-	//verticies functions
-	store.deleteVertices = function(){
-
-	}
-	store.addVertices = function(){
-
-	}
-	store.moveVertices = function(){
-
-	}
-	store.selectVertices = function(){
-
-	}
-
-	//Properties functions
-	store.updateProp = function(){
-
-	}
-	store.createProp = function(){
-
-	}
-
-	//map management
-	store.downloadGeo = function(){
-
-	}
-	store.downloadShp = function(){
-
-	}
-	store.downloadPng = function(){
-
-	}
-	store.uploadMap = function(){
-
-	}
-	store.forkMap = function(){
-
-	}
-	store.changeMapName = function(){
-
-	}
-	store.saveMap = function(){
-
-	}
-	store.deleteMap = function(){
-
-	}
-	return (
-		<GlobalStoreContext.Provider
-			value={{
-				store,
-			}}
-		>
-			{props.children}
-		</GlobalStoreContext.Provider>
-	);
+  return (
+    <GlobalStoreContext.Provider
+      value={{
+        store,
+      }}
+    >
+      {props.children}
+    </GlobalStoreContext.Provider>
+  );
 }
 
 export default GlobalStoreContext;
